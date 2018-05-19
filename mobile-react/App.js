83938a81804750eb35317a1e1ac3a134b9135de7
charles <charleslavong@gmail.com>
2018-05-19 15:59:05 -0400

    initial page

--- mobile-react/App.js
+++ mobile-react/App.js
@@ -1,23 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+//import Amplify from 'aws-amplify';
+//import aws_exports from './aws-exports';
+import Timer from './Timer'
+import LeaderBoard from './LeaderBoard'
+
 
 export default class App extends React.Component {
+  state = {
+    go: null,
+  }
+  
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        {this.state.go ? <Timer onConnect={this.handleConnect}/>
+        : <Button title='Disconnect' onPress={this.handleDisconnect}></Button> }
+        <LeaderBoard/>
       </View>
     );
+  }
+
+  handleDisconnect = () => {
+    this.setState({go: true})
+  }
+
+  handleConnect = () => {
+    this.setState({go: false})
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f1ea40',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+//Amplify.configure(aws_exports);
