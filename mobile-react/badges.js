2ce34009c28695e9096690e1accaf7ffea7137af
Sean Harte <scsharte@gmail.com>
2018-05-20 11:27:37 -0400

    badge styling

--- mobile-react/badges.js
+++ mobile-react/badges.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
-export default function Baddge() {
+export default class Baddge  extends React.Component{
+	static navigationOptions = {
+	    headerStyle: {
+	      display: 'none',
+	    },
+	  }
+	render() {
+		return (
+			<View style={styles.container} >
+				<View style={{alignItems: 'center'}}>
+					<Text style={styles.titleTextStyle}>BADGES</Text>
+				</View>
+				<View style={{flex: 2, flexDirection: 'row', flexWrap: 'wrap', marginTop: 20, }}>
+					<Image style={{width: 80, height: 90, margin: 20}} source={require('./images/basic_badge.png')} />
+					<Image style={{width: 80, height: 90, margin: 20}} source={require('./images/1star.png')} />
+					<Image style={{width: 80, height: 90, margin: 20}} source={require('./images/2star.png')} />
+					<Image style={{width: 80, height: 90, margin: 20}} source={require('./images/2star.png')} />
+				</View>
+		    </View>
+		);
+	}
 	
-	return (
-		<View style={styles.container} >
-			<View style={{alignItems: 'center'}}>
-				<Text style={styles.titleTextStyle}>BADGES</Text>
-			</View>
-			<View style={{flex: 2, flexDirection: 'row', flexWrap: 'wrap', marginTop: 20, }}>
-				<Image style={{width: 80, height: 90, margin: 20}} source={require('./images/basic_badge.png')} />
-				<Image style={{width: 80, height: 90, margin: 20}} source={require('./images/1star.png')} />
-				<Image style={{width: 80, height: 90, margin: 20}} source={require('./images/2star.png')} />
-				<Image style={{width: 80, height: 90, margin: 20}} source={require('./images/2star.png')} />
-			</View>
-	    </View>
-	);
 	
 }
 
@@ -27,6 +34,7 @@ const styles = StyleSheet.create({
 	},
 	container: {
     flex: 1,
+    paddingTop: 50,
     backgroundColor: '#9bda49',
     alignItems: 'center',
     justifyContent: 'space-between',
